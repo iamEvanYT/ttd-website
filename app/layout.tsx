cdaf10125eb37be5dd9b1c32fd2ab46bc73beb7f
iamEvan <chanevan0819@gmail.com>
2024-09-23 18:23:16 +0100

    feat: added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script defer src="https://umami.iamevan.dev/script.js" data-website-id="47d4dd0d-9720-4344-a304-c00c8d18564c"></script>
+      </head>
       <body>
         <ThemeProvider
           attribute="class"
