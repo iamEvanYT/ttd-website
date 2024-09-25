4678c725e520c130861b6b2c3012dd3bf539dcae
iamEvan <chanevan0819@gmail.com>
2024-09-25 21:18:36 +0100

    chore: remove unused table

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -15,29 +15,6 @@ interface FAQItem {
     answer: string;
 }
 
-const faqData: FAQItem[] = [
-    {
-        question: "I purchased something for Robux but I didn't get anything. What do I do?",
-        answer: "Join a new server, and you should be granted your purchase. If it's not, contact Roblox Support: [roblox.com/support](https://roblox.com/support)",
-    },
-    {
-        question: "What do I do if I got hacked?",
-        answer: `
-If someone random gain access to your Roblox Account, don't worry.
-
-Here's a guide on how to stop the hackers from further accessing your account:
-
-1. Go to Roblox Settings - [roblox.com/my/account](https://www.roblox.com/my/account)
-2. Change your password.
-3. Go to the "Security" tab.
-4. Scroll down and click "Logout of All Other Sessions".
-5. Your account is now safe again!
-
-**You need to do this _as soon as possible_, as the hacker can change your password and lock you out of your own account.**
-        `,
-    },
-]
-
 export default function FAQPage() {
     return (
         <div className="flex flex-col min-h-screen text-white">
