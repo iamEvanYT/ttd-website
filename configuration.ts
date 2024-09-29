7fdb4e5dd9ce6b769a391955d26faf68971e5646
iamEvan <47493765+iamEvanYT@users.noreply.github.com>
2024-09-29 14:26:44 +0100

    feat: update banner image

--- configuration.ts
+++ configuration.ts
@@ -1,2 +1,2 @@
 export const OPENGRAPH_SITE_NAME = "Toilet Tower Defense"
-export const BANNER_IMAGE = "https://toilettowerdefense.com/content/images/size/w1000/2024/09/DrillEventUpdate.png";
+export const BANNER_IMAGE = "/images/banner.png";
