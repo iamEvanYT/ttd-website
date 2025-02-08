528a0be8e8e745ed09507a5b03b668701f7f8e97
iamEvan <47493765+iamEvanYT@users.noreply.github.com>
2025-02-08 19:06:49 +0000

    chore: disable user profile card on post

--- components/ghost/user-profile-card.tsx
+++ components/ghost/user-profile-card.tsx
@@ -1,32 +1,43 @@
-import Image from "next/image"
+import Image from "next/image";
 
 interface UserProfileProps {
-  username: string
-  avatarSrc: string
-  bottomText?: string
+  username: string;
+  avatarSrc: string;
+  bottomText?: string;
 }
 
-export default function UserProfileCard({ username, avatarSrc, bottomText }: UserProfileProps = {
-  username: "iamEvan",
-  avatarSrc: "/placeholder.svg?height=80&width=80",
-  bottomText: "Sep 21, 2024",
-}) {
+export default function UserProfileCard(
+  { username, avatarSrc, bottomText }: UserProfileProps = {
+    username: "iamEvan",
+    avatarSrc: "/placeholder.svg?height=80&width=80",
+    bottomText: "Sep 21, 2024",
+  }
+) {
+  // Disabled
+  return <></>;
+
   return (
     <section className="article-byline-content">
       <ul className="author-list instapaper_ignore">
         <li className="author-list-item">
           <a className="author-avatar" aria-label="Read more of iamEvan">
-            <img className="author-profile-image" src={avatarSrc} alt="iamEvan" />
+            <img
+              className="author-profile-image"
+              src={avatarSrc}
+              alt="iamEvan"
+            />
           </a>
         </li>
       </ul>
 
       <div className="article-byline-meta">
-        <h4 className="author-name"><a>{username}</a></h4>
+        <h4 className="author-name">
+          <a>{username}</a>
+        </h4>
         <div className="byline-meta-content">
           {bottomText && <span>{bottomText}</span>}
         </div>
       </div>
     </section>
-  )
+  );
 }
