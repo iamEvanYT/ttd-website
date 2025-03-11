adc1ff5452fcd793b6ffe1a3756b66a6d591ebdc
iamEvan <47493765+iamEvanYT@users.noreply.github.com>
2025-03-11 18:05:11 +0000

    feat: update dockerfile to reduce size

--- Dockerfile
+++ Dockerfile
@@ -7,57 +7,45 @@ WORKDIR /app
 # Copy dependency manifests
 COPY package.json bun.lockb ./
 
-# Install dependencies using Bun
-RUN bun install --frozen-lockfile
+# Install dependencies using Bun (only production dependencies)
+RUN bun install --frozen-lockfile --production
 
-# Stage 2: Build the Application with Node.js
+# Stage 2: Build the Application with Bun
 FROM oven/bun:latest AS builder
 
 # Set working directory
 WORKDIR /app
 
-# Copy installed dependencies from deps stage
+# Copy only production dependencies from deps stage
 COPY --from=deps /app/node_modules ./node_modules
 
 # Copy the rest of the application source code
 COPY . .
 
-# Optionally disable Next.js telemetry (uncomment if desired)
-# ENV NEXT_TELEMETRY_DISABLED=1
+# Disable Next.js telemetry
+ENV NEXT_TELEMETRY_DISABLED=1
 
-# Build the application using Node.js based on the detected package manager
-RUN \
-  if [ -f yarn.lock ]; then yarn run build; \
-  elif [ -f package-lock.json ]; then npm run build; \
-  elif [ -f pnpm-lock.yaml ]; then pnpm run build; \
-  elif [ -f bun.lockb ]; then bun run build; \
-  else echo "Lockfile not found." && exit 1; \
-  fi
+# Build the application using Bun
+RUN bun run build
 
-# Stage 3: Create Production Image with Node.js
-FROM node:18-alpine AS runner
+# Stage 3: Create Production Image with Bun (smaller than Node)
+FROM oven/bun:latest AS runner
 
 # Set working directory
 WORKDIR /app
 
 # Set environment to production
 ENV NODE_ENV=production
-
-# Optionally disable Next.js telemetry during runtime (uncomment if desired)
-# ENV NEXT_TELEMETRY_DISABLED=1
+ENV NEXT_TELEMETRY_DISABLED=1
 
 # Create a non-root user and group for running the application
-RUN addgroup --system --gid 1001 nodejs \
+RUN addgroup --system --gid 1001 bunjs \
     && adduser --system --uid 1001 nextjs
 
-# Copy public assets from the builder stage
-COPY --from=builder /app/public ./public
-
-# Create and set permissions for the prerender cache directory
-RUN mkdir .next && chown nextjs:nodejs .next
-
-# Leverage Next.js output tracing to reduce image size by copying only necessary files
-COPY --from=builder --chown=nextjs:nodejs /app .
+# Copy only the necessary files from the builder stage
+COPY --from=builder --chown=nextjs:bunjs /app/public ./public
+COPY --from=builder --chown=nextjs:bunjs /app/.next/standalone ./
+COPY --from=builder --chown=nextjs:bunjs /app/.next/static ./.next/static
 
 # Switch to the non-root user
 USER nextjs
@@ -69,5 +57,5 @@ EXPOSE 3000
 ENV PORT=3000
 ENV HOSTNAME="0.0.0.0"
 
-# Define the command to start the application using Node.js
-CMD ["npm", "start"]
+# Define the command to start the application using Bun
+CMD ["bun", "server.js"]
--- next.config.mjs
+++ next.config.mjs
@@ -1,63 +1,64 @@
 const oldPostSlugs = [
-    "drill-event-update",
-    "rewards-update",
-    "pencil-event-update",
-    "gadget-crate-update",
-    "clans-update",
-    "episode-77-update-part-1",
-    "food-crate-update",
-    "abysmal-mode-update",
-    "episode-76-part-2-update",
-    "episode-76-part-1-update",
-    "episode-75-update",
-    "rewind-event-part-2",
-    "roblox-classic-update",
-]
+  "drill-event-update",
+  "rewards-update",
+  "pencil-event-update",
+  "gadget-crate-update",
+  "clans-update",
+  "episode-77-update-part-1",
+  "food-crate-update",
+  "abysmal-mode-update",
+  "episode-76-part-2-update",
+  "episode-76-part-1-update",
+  "episode-75-update",
+  "rewind-event-part-2",
+  "roblox-classic-update",
+];
 
 const extraRedirects = {
-    // '/game': "https://www.roblox.com/games/13775256536/",
-    // '/discord': "https://discord.com/invite/SrnQt2yDeZ",
-}
+  // '/game': "https://www.roblox.com/games/13775256536/",
+  // '/discord': "https://discord.com/invite/SrnQt2yDeZ",
+};
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'api.toilettowerdefense.com',
-                port: '',
-                pathname: '/image/*',
-            },
-            {
-                protocol: 'https',
-                hostname: 'blog.toilettowerdefense.com',
-                port: '',
-                pathname: '/content/images/**',
-            },
-        ],
-    },
-    async redirects() {
-        const redirects = []
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.toilettowerdefense.com",
+        port: "",
+        pathname: "/image/*",
+      },
+      {
+        protocol: "https",
+        hostname: "blog.toilettowerdefense.com",
+        port: "",
+        pathname: "/content/images/**",
+      },
+    ],
+  },
+  output: "standalone",
+  async redirects() {
+    const redirects = [];
 
-        oldPostSlugs.forEach((slug) => {
-            redirects.push({
-                source: `/${slug}`,
-                destination: `/post/${slug}`,
-                permanent: true,
-            })
-        })
+    oldPostSlugs.forEach((slug) => {
+      redirects.push({
+        source: `/${slug}`,
+        destination: `/post/${slug}`,
+        permanent: true,
+      });
+    });
 
-        Object.entries(extraRedirects).forEach(([source, destination]) => (
-            redirects.push({
-                source,
-                destination,
-                permanent: true
-            })
-        ))
+    Object.entries(extraRedirects).forEach(([source, destination]) =>
+      redirects.push({
+        source,
+        destination,
+        permanent: true,
+      })
+    );
 
-        return redirects
-    },
+    return redirects;
+  },
 };
 
 export default nextConfig;
